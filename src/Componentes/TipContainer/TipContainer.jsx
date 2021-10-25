3f5aa633246000be202a83dd9bf1ceed03c17920
Wenceslaorojas <wenceslao.rojar@gmail.com>
2021-10-25 16:27:53 -0300

    app terminada

---
+++ src/Componentes/TipContainer/TipContainer.jsx
@@ -0,0 +1,272 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import tipStayle from "./TipContainer.module.css";
+
+function TipContainer() {
+  // Declaro mis constantes de estado
+  const [bill, setBill] = useState();
+  const [tip, setTip] = useState(0);
+  const [person, setPerson] = useState(1);
+  const [totalTip, setTotalTip] = useState("0.00");
+  const [tipPerson, setTipPerson] = useState("0.00");
+
+  //declaro un initial state seguido de su constate de estado
+  var initialStateActives = [
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+  ];
+  const [selectBtn, setSelectBtn] = useState(initialStateActives);
+  // array y funcion switch para controlar el boton de propina activo
+  var actives = [
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+    {
+      border: "none",
+    },
+  ];
+  const btnActive = (e) => {
+    switch (e.target.value) {
+      case "5":
+        actives = [...initialStateActives];
+        actives[0] = {
+          backgroundColor: "hsl(172, 67%, 45%)",
+        };
+        setSelectBtn(actives);
+        break;
+      case "10":
+        actives = [...initialStateActives];
+        actives[1] = {
+          backgroundColor: "hsl(172, 67%, 45%)",
+        };
+        setSelectBtn(actives);
+        break;
+      case "15":
+        actives = [...initialStateActives];
+        actives[2] = {
+          backgroundColor: "hsl(172, 67%, 45%)",
+        };
+        setSelectBtn(actives);
+        break;
+      case "25":
+        actives = [...initialStateActives];
+        actives[3] = {
+          backgroundColor: "hsl(172, 67%, 45%)",
+        };
+        setSelectBtn(actives);
+        break;
+      case "50":
+        actives = [...initialStateActives];
+        actives[4] = {
+          backgroundColor: "hsl(172, 67%, 45%)",
+        };
+        setSelectBtn(actives);
+        break;
+      default:
+        actives = [...initialStateActives];
+        actives[5] = {
+          border: "2px solid #26c0ab",
+        };
+        setSelectBtn(actives);
+    }
+  };
+  // funcion que calcula el valor de la propina total y propina por persona
+  function totalTipCalculate(person1, tip1, bill1) {
+    if (!tip1 || !bill1 || !person1) {
+      tip1 = tip;
+      bill1 = bill;
+      person1 = person;
+    }
+    let result = ((tip1 * bill1) / 100).toFixed(2);
+    if (isNaN(result)) {
+      setTotalTip("0.00");
+      setTipPerson("0.00");
+      return;
+    }
+    setTotalTip(result);
+    setTipPerson((result / person1).toFixed(2));
+  }
+  // funcion que resetea los valores y los inputs
+  const reset = (e) => {
+    console.log(e);
+
+    setSelectBtn(initialStateActives);
+    setPerson(1);
+    setTipPerson("0.00");
+    setBill(0);
+    setTip(0);
+    setTotalTip("0.00");
+  };
+  // actualizamos todos los estados con cualquier cambio de input
+  useEffect(() => {
+    var tip1 = tip;
+    var person1 = person;
+    var bill1 = bill;
+    totalTipCalculate(person1, tip1, bill1);
+  }, [tip, bill, person]);
+
+  return (
+    <div className={tipStayle.tipContainer}>
+      {/* SECCION DE INPUTS */}
+      <div className={tipStayle.inputsContainer}>
+        <div className={tipStayle.billContainer}>
+          <span> Bill </span>
+          <input
+            value={bill}
+            type="number"
+            placeholder="0"
+            min="0"
+            onChange={(e) => {
+              setBill(e.target.value);
+              totalTipCalculate();
+            }}
+          />
+        </div>
+        <div className={tipStayle.btnsContainer}>
+          <span>Select Tip %</span>
+          <div className={tipStayle.divBtn1}>
+            <button
+              className={tipStayle.btnTip}
+              style={selectBtn[0]}
+              value="5"
+              onClick={(e) => {
+                btnActive(e);
+                setTip(e.target.value);
+                totalTipCalculate();
+              }}
+            >
+              5%
+            </button>
+            <button
+              className={tipStayle.btnTip}
+              style={selectBtn[1]}
+              value="10"
+              onClick={(e) => {
+                btnActive(e);
+                setTip(e.target.value);
+                totalTipCalculate();
+              }}
+            >
+              10%
+            </button>
+            <button
+              className={tipStayle.btnTip}
+              style={selectBtn[2]}
+              value="15"
+              onClick={(e) => {
+                btnActive(e);
+                setTip(e.target.value);
+                totalTipCalculate();
+              }}
+            >
+              15%
+            </button>
+          </div>
+          <div className={tipStayle.divBtn2}>
+            <button
+              className={tipStayle.btnTip}
+              value="25"
+              style={selectBtn[3]}
+              onClick={(e) => {
+                btnActive(e);
+                setTip(e.target.value);
+                totalTipCalculate();
+              }}
+            >
+              25%
+            </button>
+            <button
+              className={tipStayle.btnTip}
+              style={selectBtn[4]}
+              value="50"
+              onClick={(e) => {
+                btnActive(e);
+                setTip(e.target.value);
+                totalTipCalculate();
+              }}
+            >
+              50%
+            </button>
+            <input
+              type="number"
+              placeholder="Custom"
+              style={selectBtn[5]}
+              min="0"
+              className={tipStayle.customTip}
+              onChange={(e) => {
+                btnActive(e);
+                setTip(e.target.value);
+                totalTipCalculate();
+              }}
+            />
+          </div>
+        </div>
+        <div className={tipStayle.peopleContainer}>
+          <span className={tipStayle.peopleSpan}>Number of People</span>
+          <input
+            value={person}
+            type="number"
+            placeholder="0"
+            style={selectBtn[6]}
+            min="0"
+            className={tipStayle.peopleBtn}
+            onChange={(e) => {
+              if (e.target.value.length === 0) {
+                setPerson(1);
+                totalTipCalculate();
+                return;
+              }
+              setPerson(e.target.value);
+              totalTipCalculate();
+            }}
+          />
+        </div>
+      </div>
+      {/* ---------------------------------------------------- */}
+      {/* Seccion Results */}
+      <div className={tipStayle.resultsContainer}>
+        <div>
+          <span className={tipStayle.spanTotal}>Total Tip</span>
+          <span className={tipStayle.spanMoney}> $ {totalTip} </span>
+        </div>
+        <div>
+          <span className={tipStayle.spanTotal}>/Person</span>
+          <span className={tipStayle.spanMoney}> $ {tipPerson} </span>
+        </div>
+        <button className={tipStayle.btn} onClick={(e) => reset(e)}>
+          RESET
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default TipContainer;
